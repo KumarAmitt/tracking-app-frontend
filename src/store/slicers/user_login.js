077d5e19c76f7664a7055dfaf9b8b82c1362d102
KumarAmitt <amitt2015@outlook.com>
2021-08-22 20:12:44 +0530

    Add user_login slicer

---
+++ src/store/slicers/user_login.js
@@ -0,0 +1,60 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { createSelector } from 'reselect';
+import { apiCallBegan } from '../api';
+
+const loginSlice = createSlice({
+  name: 'login',
+  initialState: {
+    loggedInUser: {},
+    loading: false
+  },
+  reducers: {
+    loginInitiated: (state, action) => {
+      state.loading = true;
+    },
+    userLogin: (state, action) => {
+      if(action.payload.status === 401){
+        state.loggedInUser.status = action.payload.status
+      }else{
+        state.loggedInUser.id = action.payload.user.id;
+        state.loggedInUser.username = action.payload.user.username;
+        state.loggedInUser.status = action.payload.status;
+        state.loggedInUser.logged_in = action.payload.logged_in;
+      }
+      state.loading = false;
+    },
+    loginFailed: (state, action) => {
+      state.loading = false;
+    },
+  },
+});
+
+const { userLogin, loginInitiated, loginFailed } = loginSlice.actions;
+export default loginSlice.reducer;
+
+// ACTION CREATOR
+
+export const loginUser = (user) => apiCallBegan({
+  url: '/sessions',
+  method: 'post',
+  data: user,
+  withCredentials: true,
+  onStart: loginInitiated.type,
+  onSuccess: userLogin.type,
+  onError: loginFailed.type,
+});
+
+
+// SELECTOR
+
+export const getLoggedInStatus = createSelector(
+    (state) => state.entities.auth.login.loggedInUser.status,
+    (status) => status === 'created'
+);
+
+// SELECTOR
+
+export const getLoginInfo = createSelector(
+    (state) => state.entities.auth.login.loggedInUser,
+    (loggedInUser) => loggedInUser.status === 'created' ? loggedInUser : false
+);
