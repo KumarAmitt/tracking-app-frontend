839549f6d250f93c454475a3ea21c42b2a05656e
KumarAmitt <amitt2015@outlook.com>
2021-08-22 22:03:30 +0530

    Make login component functional

--- src/App/auth/Login.js
+++ src/App/auth/Login.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getLoggedInStatus, loginUser } from '../../store/slicers/user_login';
 
-const Login = () => (
-  <>
-    <h2>Sign In</h2>
-    <form>
-      <input type="text" name="username" placeholder="Username" required />
-      <input type="password" name="password" placeholder="Password" required />
-      <button type="submit">Login</button>
-    </form>
-    <br />
-    <p>New User Registration form</p>
-    <Link to="/registration">Sign Up</Link>
-  </>
-);
+const Login = () => {
+  const dispatch = useDispatch();
+  const loggedInStatus = useSelector(getLoggedInStatus);
+  const [newUser, setNewUser] = useState({ username: '', password: '' });
+
+  const handleChange = (e) => {
+    setNewUser({ ...newUser, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(loginUser({
+      user: {
+        username: newUser.username,
+        password: newUser.password,
+      },
+    }));
+  };
+
+  if (loggedInStatus) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <>
+      <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="Username" value={newUser.username} onChange={handleChange} required />
+        <input type="password" name="password" placeholder="Password" value={newUser.password} onChange={handleChange} required />
+        <button type="submit">Login</button>
+      </form>
+      <br />
+      <p>New User Registration form</p>
+      <Link to="/registration">Sign Up</Link>
+    </>
+  );
+};
 
 export default Login;
