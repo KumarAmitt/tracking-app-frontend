aa6dfa76289426049c3a6809c27a73efc5f7747d
KumarAmitt <amitt2015@outlook.com>
2021-08-22 20:14:36 +0530

    Add user_registration slicer

---
+++ src/store/slicers/user_registration.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { createSelector } from 'reselect';
+import { apiCallBegan } from '../api';
+
+const registrationSlice = createSlice({
+  name: 'registration',
+  initialState: {
+    registration: {},
+    loading: false
+  },
+  reducers: {
+    registrationInitiated: (state, action) => {
+      state.loading = true;
+    },
+    userRegistered: (state, action) => {
+      console.log(action.payload.status)
+      state.registration.id = action.payload.user.id;
+      state.registration.username = action.payload.user.username;
+      state.registration.password = action.payload.user['password_digest'];
+      state.registration.status = action.payload.status;
+      state.loading = false;
+    },
+    registrationFailed: (state, action) => {
+      state.registration.error = action.payload
+      state.loading = false;
+    }
+  },
+});
+
+const { userRegistered, registrationInitiated, registrationFailed} = registrationSlice.actions;
+export default registrationSlice.reducer;
+
+// ACTION CREATOR
+
+export const registerUser = (user) => apiCallBegan({
+  url: '/registrations',
+  method: 'post',
+  data: user,
+  withCredentials: true,
+  onStart: registrationInitiated.type,
+  onSuccess: userRegistered.type,
+  onError: registrationFailed.type,
+});
+
+
+// SELECTOR
+
+export const getRegistrationStatus = createSelector(
+    (state) => state.entities.auth.registration.registration.status,
+    (status) => status === 'created',
+);
+
+export const getRegistrationInfo = createSelector(
+    (state) => state.entities.auth.registration.registration,
+    (registration) => registration.status === 'created' ? registration : false
+);
+
