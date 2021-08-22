ff1d6b0ae66a6bbd749d60adedc194eee409e864
KumarAmitt <amitt2015@outlook.com>
2021-08-22 22:09:14 +0530

    Load session Info at dashboard

--- src/App/components/Dashboard.js
+++ src/App/components/Dashboard.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkLoginStatus, getSessionInfo } from '../../store/slicers/user_session';
 
-const Dashboard = () => (
-  <>
-    <h1>Dashboard</h1>
-  </>
-);
+const Dashboard = () => {
+  const dispatch = useDispatch();
+  const sessionInfo = useSelector(getSessionInfo);
+
+  useEffect(() => {
+    dispatch(checkLoginStatus());
+  });
+
+  console.log(sessionInfo);
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+    </>
+  );
+};
 
 export default Dashboard;
