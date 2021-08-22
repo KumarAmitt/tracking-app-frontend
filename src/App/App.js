647337c40a8c70b18fafa0bae8f711f95b5358af
KumarAmitt <amitt2015@outlook.com>
2021-08-22 20:47:30 +0530

    Add Routes to App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './auth/Home';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Tracking App</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
