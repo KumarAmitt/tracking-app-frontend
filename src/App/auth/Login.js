9f876f17cdb8bc9853efc0d943e9d22ed9a50929
KumarAmitt <amitt2015@outlook.com>
2021-08-22 20:34:53 +0530

    Add functional component template to files in App directory

---
+++ src/App/auth/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => (
+  <>
+    <h1>Home</h1>
+  </>
+);
+
+export default Login;
---
+++ src/App/auth/Registration.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Registration = () => (
+  <>
+    <h1>Home</h1>
+  </>
+);
+
+export default Registration;
