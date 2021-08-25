215020705baedd635938f5fabd6bd3471ad7ecc6
KumarAmitt <amitt2015@outlook.com>
2021-08-25 21:32:30 +0530

    Make the items at Track tab as link

--- src/App/components/Track.js
+++ src/App/components/Track.js
@@ -3,49 +3,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import AppBar from './AppBar';
 import { getAllDeals, loadDeals } from '../../store/slicers/deal';
-import { getProducts, loadProducts } from '../../store/slicers/product';
 
 const Track = () => {
   const title = 'Track Your Deal';
   const dispatch = useDispatch();
   const deals = useSelector(getAllDeals);
-  const products = useSelector(getProducts);
 
   useEffect(() => {
     dispatch(loadDeals());
-    dispatch(loadProducts());
   }, []);
 
-  console.log(products);
-  console.log(deals);
-
-  const handleClick = (data) => {
-    console.log(data);
-  };
-
-  // products.length > 0 ? products.filter((f) => f.id === e.product_id)[0].product_name : ''
+  // console.log(products);
+  // console.log(deals);
 
   return (
     <>
       <AppBar title={title} />
       {
         Object.entries(deals).map((d) => (
-          <div key={d[0]} onClick={() => handleClick(d[1])} onKeyDown={() => handleClick(d[1])} role="button" tabIndex={0}>
-            <span>
-              {d[0]}
-              :
-            </span>
-            {' '}
-            <span>{d[1].map((e) => e.premium).reduce((a, b) => a + b)}</span>
-            {' '}
-            <span>{d[1].length}</span>
-          </div>
-        ))
-      }
-
-      {
-        Object.entries(deals).map((d) => (
-          <Link to={{ pathname: '/track_details', info: d[1] }} key={d[0]}>
+          <Link to={{ pathname: '/track_details', info: d[1], date: d[0] }} key={d[0]}>
             <div>
               <span>
                 {d[0]}
--- src/App/components/TrackDetails.js
+++ src/App/components/TrackDetails.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import AppBar from './AppBar';
+import { getProducts, loadProducts } from '../../store/slicers/product';
 
 const TrackDetails = ({ location }) => {
+  const { info, date } = location;
   const title = 'Track Details';
+  const dispatch = useDispatch();
+  const products = useSelector(getProducts);
+  const todayTotal = info ? info.map((e) => e.premium).reduce((a, b) => a + b) : 0;
 
-  console.log(location.info);
+  useEffect(() => {
+    dispatch(loadProducts());
+  }, []);
+
+  console.log(todayTotal);
   return (
     <>
       <AppBar title={title} />
       <Link to="/track">Go Back</Link>
+      <div>{date}</div>
+      <hr />
+      {
+        info && info.map((e) => (
+          <div key={e.id}>
+            <div>{products.length > 0 ? products.filter((f) => f.id === e.product_id)[0].product_name : ''}</div>
+            <div>{e.premium}</div>
+            <div>{e.application_id}</div>
+            <hr />
+          </div>
+        ))
+      }
     </>
   );
 };
