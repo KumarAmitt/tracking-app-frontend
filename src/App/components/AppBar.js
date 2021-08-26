e2f2c3e7f8a54487916a5de0d3b5d1f29868e63c
KumarAmitt <amitt2015@outlook.com>
2021-08-27 01:23:48 +0530

    Style AppBar component

--- src/App/components/AppBar.js
+++ src/App/components/AppBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import './style/AppBar.css';
+import ArrowBackIosOutlinedIcon from '@material-ui/icons/ArrowBackIosOutlined';
 
 const AppBar = ({ title, link }) => {
   let linkText = '';
@@ -11,12 +13,15 @@ const AppBar = ({ title, link }) => {
     default:
       linkText = 'HOME';
   }
+  console.log(linkText);
 
   return (
     <>
-      <div style={{ backgroundColor: 'blue', color: '#fff' }}>
-        <Link to={link}>{linkText}</Link>
-        <h2>{title}</h2>
+      <div className="appbar">
+        <Link to={link}>
+          <ArrowBackIosOutlinedIcon />
+        </Link>
+        <div className="title">{title}</div>
       </div>
     </>
   );
