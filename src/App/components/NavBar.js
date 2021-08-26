b12ebf5819713c3ecd043e29831978316383bbfd
KumarAmitt <amitt2015@outlook.com>
2021-08-27 00:42:44 +0530

    Add icons in nav bar

--- src/App/components/NavBar.js
+++ src/App/components/NavBar.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AddOutlinedIcon from '@material-ui/icons/AddOutlined';
+import TrendingUpOutlinedIcon from '@material-ui/icons/TrendingUpOutlined';
+import TrackChangesOutlinedIcon from '@material-ui/icons/TrackChangesOutlined';
+import PersonOutlinedIcon from '@material-ui/icons/PersonOutlined';
 import './style/NavBar.css';
 
 const NavBar = () => (
   <div className="navbar">
-    <Link to="/add_deal">Add Deal</Link>
-    <Link to="/track">Track</Link>
-    <Link to="/progress">Progress</Link>
-    <Link to="/profile">Profile</Link>
+    <Link to="/add_deal">
+      <AddOutlinedIcon />
+      <div>Add Deal</div>
+    </Link>
+    <Link to="/track">
+      <TrendingUpOutlinedIcon />
+      <div>Track</div>
+    </Link>
+    <Link to="/progress">
+      <TrackChangesOutlinedIcon />
+      <div>Progress</div>
+    </Link>
+    <Link to="/profile">
+      <PersonOutlinedIcon />
+      <div>Profile</div>
+    </Link>
   </div>
 );
 
