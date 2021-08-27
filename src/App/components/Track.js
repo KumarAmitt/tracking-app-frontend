af82970ba5a92583222ae01215cb3fc4e8fec2ac
KumarAmitt <amitt2015@outlook.com>
2021-08-27 15:24:28 +0530

    Restructure date to be displayed on Track tile

--- src/App/components/Track.js
+++ src/App/components/Track.js
@@ -4,6 +4,7 @@ import { Link, Redirect } from 'react-router-dom';
 import AppBar from './AppBar';
 import { getAllDeals, loadDeals } from '../../store/slicers/deal';
 import { getSessionInfo, loadSession } from '../../store/slicers/user_session';
+import './style/Track.css';
 
 const Track = () => {
   const title = 'Track Your Deal';
@@ -25,26 +26,29 @@ const Track = () => {
   }
 
   return (
-    <>
+    <div className="track">
       <AppBar title={title} />
       {
-        Object.entries(deals).length === 0 ? 'No data found. Please Add deal first'
+        Object.entries(deals).length === 0 ? <div className="not-found">No data found. Please Add deal first</div>
           : Object.entries(deals).map((d) => (
-            <Link to={{ pathname: '/track_details', info: d[1], date: d[0] }} key={d[0]}>
-              <div>
-                <span>
-                  {d[0]}
-                  :
-                </span>
-                {' '}
-                <span>{d[1].map((e) => e.premium).reduce((a, b) => a + b)}</span>
-                {' '}
-                <span>{d[1].length}</span>
-              </div>
-            </Link>
+            <div key={d[0]} className="track-tile">
+              <Link to={{ pathname: '/track_details', info: d[1], date: d[0] }}>
+                <div className="track-tile-left">
+                  <div className="date">{d[0]}</div>
+                  <div className="date">
+                    Policy Count:
+                    {' '}
+                    <span>{d[1].length}</span>
+                  </div>
+                </div>
+                <div className="track-tile-right">
+                  {d[1].map((e) => e.premium).reduce((a, b) => a + b)}
+                </div>
+              </Link>
+            </div>
           ))
        }
-    </>
+    </div>
   );
 };
 
