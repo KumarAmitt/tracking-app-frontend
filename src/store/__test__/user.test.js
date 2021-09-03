93311473f05623c80b20aea5b737ba7875a10cfc
KumarAmitt <amitt2015@outlook.com>
2021-09-04 01:47:45 +0530

    Add tests for selectors in userSlice

--- src/store/__test__/user.test.js
+++ src/store/__test__/user.test.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import configureAppStore from '../configureStore';
 import {
-  registerUser, loginUser, loadSession, logoutUser,
+  registerUser, loginUser, loadSession, logoutUser, getUserInfo, getUserLoadingStatus,
 } from '../slicers/user';
 
 describe('Existing user Login', () => {
@@ -16,21 +16,21 @@ describe('Existing user Login', () => {
 
   const userSlice = () => store.getState().entities.user;
 
-  describe('New user registration', () => {
-    const registrationCredentials = {
-      user: {
-        username: 'user1',
-        password: 'asdf',
-        password_confirmation: 'asdf',
-      },
-    };
-
-    const registrationResponse = {
-      status: 200,
-      logged_in: true,
+  const registrationCredentials = {
+    user: {
       username: 'user1',
-    };
+      password: 'asdf',
+      password_confirmation: 'asdf',
+    },
+  };
 
+  const registrationResponse = {
+    status: 200,
+    logged_in: true,
+    username: 'user1',
+  };
+
+  describe('New user registration', () => {
     describe('registerUser', () => {
       it('should return the created user on successful registration', async () => {
         fakeAxios.onPost('/registrations').reply(200, registrationResponse);
@@ -124,8 +124,74 @@ describe('Existing user Login', () => {
         fakeAxios.onDelete('/logout').reply(500);
 
         await store.dispatch(logoutUser());
-        console.log(userSlice());
+
         expect(userSlice().info).toMatchObject({});
+      });
+    });
+  });
+
+  describe('loading Indicator', () => {
+    it('should return true while creating new user', () => {
+      fakeAxios.onPost('/registrations').reply(() => {
+        expect(userSlice().loading).toBe(true);
+        return [200, registrationResponse];
+      });
+      store.dispatch(registerUser(registrationCredentials));
+    });
+
+    it('should be false after loading session', async () => {
+      fakeAxios.onGet('/sessions').reply(200, { logged_in: true });
+
+      await store.dispatch(loadSession());
+
+      expect(userSlice().loading).toBe(false);
+    });
+
+    it('should be false if session loading failed', async () => {
+      fakeAxios.onGet('/logged_in').reply(500);
+
+      await store.dispatch(loadSession());
+
+      expect(userSlice().loading).toBe(false);
+    });
+  });
+
+  describe('selectors', () => {
+    const createState = () => ({
+      entities: {
+        user: {
+          info: {},
+          loading: false,
+        },
+      },
+    });
+
+    let state;
+
+    beforeEach(() => {
+      state = createState();
+    });
+
+    describe('getUserInfo', () => {
+      it('returns the username with status code and logged_in key when user is created', () => {
+        state.entities.user.info = { status: 200, logged_in: true, username: 'user1' };
+
+        const result = getUserInfo(state);
+        expect(result.status).toBe(200);
+        expect(result.logged_in).toBeTruthy();
+        expect(result.username).toBe('user1');
+      });
+    });
+
+    describe('getUserLoadingStatus', () => {
+      it('return TRUE if loading state is true', () => {
+        state.entities.user.loading = true;
+        const result = getUserLoadingStatus(state);
+        expect(result).toBeTruthy();
+      });
+      it('return FALSE if loading state is true', () => {
+        const result = getUserLoadingStatus(state);
+        expect(result).toBeFalsy();
       });
     });
   });
