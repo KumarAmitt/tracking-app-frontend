325c0458bc4a1149d1379f421b410fe8ce603ae1
KumarAmitt <amitt2015@outlook.com>
2021-09-12 15:02:16 +0530

    Update Api ul to production

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const TARGET = 700000;
-// export const BASE_URL = 'https://deals-tracker.herokuapp.com';
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'https://deals-tracker.herokuapp.com';
+// export const BASE_URL = 'http://localhost:3001';
